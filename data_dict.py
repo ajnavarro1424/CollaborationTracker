1d9c8ea123e3730f9cd6a9a31cfdb30e6582f192
Matthew Bosworth <mbosworth@neuropace.com>
2017-04-17 10:45:09 -0700

    Need the data dict!

---
+++ data_dict.py
@@ -0,0 +1,47 @@
+''' Data dictionary:  one row per column in the original spreadsheet
+    THE NAME HERE MUST MATCH A NAME IN data_dict.js !!
+    name: display name; note that a blank name will be skipped
+    orig_col: The column name from the original spreadsheet
+    '''
+
+study_dict = [
+    {'name': '', 'orig_col': 'Data Sharing Req'},
+    {'name': '', 'orig_col': 'Tag #'},
+    {'name': 'NEW NEW TAG*'},
+    {'name': '', 'orig_col': 'New interim Tag'},
+    {'name': '', 'orig_col': 'ORIG TAG (CT Log)'},
+    {'name': '', 'orig_col': 'ORIG TAG (Inv Init Log)'},
+    {'name': 'Entry date'},
+    {'name': 'Entered by'},
+    {'name': 'Date Needed'},
+    {'name': 'NP Study Sharing Approval Date'},
+    {'name': 'NP Sharing Approval By'},
+    {'name': 'Sharing Status'},
+    {'name': 'NP Responsible'},
+    {'name': 'Institution handle'},
+    {'name': 'PI / Contact'},
+    {'name': 'Reason for Sharing'},
+    {'name': 'Data Sharing Method*'},
+    {'name': 'Data Set Description'},
+    {'name': 'PHI Present'},
+    {'name': 'Share Type (NP IDE/PMA or Investigator Initiated)'},
+    {'name': 'ICF Coverage'},
+    {'name': 'Study Type'},
+    {'name': 'Study Identifier'},
+    {'name': 'Study Risk Level'},
+    {'name': 'Protocol Title'},
+    {'name': 'Initial IRB App Date'},
+    {'name': 'Latest IRB Exp Date'},
+    {'name': 'Needs Research Accessories'},
+    {'name': 'Single or Multi-Ctr'},
+    {'name': '', 'orig_col': 'Inv Sites Approval Required'},
+    {'name': '', 'orig_col': 'VPN access?'},
+    {'name': 'Project Category'},
+    {'name': 'Funding Source'},
+    {'name': 'Compensation ($) '},
+    {'name': 'Consultant'},
+    {'name': 'Contract Needed'},
+    {'name': 'Contract status'},
+    {'name': 'Contract Approval Date'},
+    {'name': 'BOX link'},
+    {'name': 'Notes', 'width': 1000}]
