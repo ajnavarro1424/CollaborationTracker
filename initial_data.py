53e8bd5b5ec0b7a9e25c5033a1b9e547d651e48f
Matthew Bosworth <mbosworth@neuropace.com>
2017-04-18 11:23:53 -0700

    Got the latest version of the initial data csv Updated initial_data.py and data_dict.py to work with it Added flask_mongoengine to requirements

--- initial_data.py
+++ initial_data.py
@@ -1,24 +1,36 @@
 import csv
-from data_sharing import app, data_dict
-# from . import app
+import pymongo
+import data_dict
 
 
 def import_data():
     # First sheet: data_sharing for studies
-    app.mdb.drop_collection('clinical_studies')
-    c1 = app.mdb.clinical_studies
-    top_rows = 12  # The first 12 rows have no data??
+    db = pymongo.MongoClient().data_sharing  # assume localhost
+    db.drop_collection('clinical_studies')
+    c1 = db.clinical_studies
+    top_rows = 14  # The first 12 rows have no data??
 
     with open('DataSharingSpreadsheet.csv', 'rU') as csv_sheet:
         for (row_num, row) in enumerate(csv.reader(csv_sheet, delimiter=',')):
             if row_num < top_rows:  # skip the column names
                 continue
-            idx = 0
             sheet_dict = {}
-            for key in data_dict.study_dict:
-                if key['name']:  # skip certain columns
+            for idx, key in enumerate(data_dict.study_dict):
+                if key['name'] == '':  # skip certain columns
+                    continue
+                if '/' in key['name']:  # split this column...
+                    # this is pretty horrible
+                    # values are separated by commas, column names by slashes
+                    # sometimes there are enough values, otherwise use ''
+                    vals = row[idx].strip().split(',')
+                    for kidx, k in enumerate(key['name'].split('/')):
+                        if len(vals) < kidx:
+                            sheet_dict[k] = vals[kidx]
+                        else:  # fill with a blank val
+                            sheet_dict[k] = ''
+
+                else:  # simple case
                     sheet_dict[key['name']] = row[idx].strip()
-                idx += 1
             c1.insert_one(sheet_dict)
 
 
