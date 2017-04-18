71bc02debd8d26ca90688627b057831f675e5a72
ajavarro1424 <ajnavarro1424@gmail.com>
2017-04-18 14:47:09 -0700

    DB Setup w/Models

--- NeuropaceInternship/app.py
+++ NeuropaceInternship/app.py
@@ -1,13 +1,71 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
+
+from flask_mongoengine.wtf import model_form
+
+
+
 app = Flask(__name__)
+
+# Pymongo setup
+# client = MongoClient('localhost', 27017)
+# app.mdb = client.test_database
+# collaborations = app.mdb.collaborations
+#MongoEngine setup
+mdb = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(mdb)
+# mdb.init_app(app)
+
+
+class Collaboration(mdb.Document):
+    entry_date = mdb.DateTimeField(required = True, label = 'Entry Date')
+    entered_by = mdb.StringField(required = True)
+    institution_contact = mdb.StringField(required = True)
+    pi = mdb.StringField(required = True)
+    submit = SubmitField(label='Submit Initiation')
+
+CollabForm = model_form(
+    Collaboration,
+    only = ['entry_date', 'entered_by', 'institution_contact', 'pi']
+
+)
+
+
 
 @app.route("/")
 def main():
+
+
     return render_template('index.html')
 
 
+@app.route("/init", methods=["GET","POST"])
+def initiation():
+    form = CollabForm()
+    # if request.method == 'POST' and form.validate():
+        # form is valid, proceed to next WF step
+        # redirect('/details')
+    return render_template('init.html', form=form)
 
+@app.route("/details")
+def details():pass
+
+
+@app.route("/contract")
+def contract():pass
+
+@app.route("/legal")
+def legal():pass
+
+@app.route("/sharing")
+def sharing():pass
+
+@app.route("/closure")
+def closure():pass
 
 
 if __name__ == "__main__":
+    app.config.update(DEBUG = True, SECRET_KEY = '')
+
     app.run()
