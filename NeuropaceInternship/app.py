89dd683a3d4a93e8ca1f960859b7532d5744cc17
lpbauer <lukephilip@gmail.com>
2017-04-21 16:21:08 -0700

    Details form view populates with labels

--- NeuropaceInternship/app.py
+++ NeuropaceInternship/app.py
@@ -23,10 +23,71 @@ app.session_interface = MongoEngineSessionInterface(mdb)
 # Model used to build our collection by passing our documents (document object)
 class Collaboration(mdb.Document):
     #Collaboration class will contain all the fields for all WF
+    # Initiate Form values
     entry_date = mdb.DateTimeField()
     entered_by = mdb.StringField()
+    date_needed = mdb.DateTimeField()
     institution_contact = mdb.StringField()
     pi = mdb.StringField()
+    reason = mdb.StringField() #dropdown-menu
+    category = mdb.StringField() #dropdown-menu
+    init_comments = mdb.StringField()
+    # Details workflow (Flow2/Pg1 is Green-light or Red-light)
+    neuropace_contact = mdb.StringField() #dropdown-menu
+    sharing_method = mdb.StringField() #dropdown-menu
+    study_title = mdb.StringField()
+    dataset_description = mdb.StringField() #dropdown-menu
+    phi_present = mdb.BooleanField()
+    share_type = mdb.StringField() #dropdown-menu
+    study_type = mdb.StringField() #dropdown-menu
+    study_identifier = mdb.StringField()
+    risk_level = mdb.StringField() #dropdown-menu
+    accessories_needed = mdb.BooleanField()
+    accessories_needed_language = mdb.StringField() #dropdown-menu
+    single_multi_center = mdb.StringField() #dropdown-menu
+    detail_comments = mdb.StringField()
+    #Contracts and Financials (Flow2/Pg3)
+    funding_source = mdb.StringField() #dropdown-menu
+    np_compensation = mdb.BooleanField()
+    np_consultant = mdb.BooleanField()
+    contract_needed = mdb.StringField()
+    budget_needed = mdb.StringField()
+    contract_notes = mdb.StringField()
+    #Legal
+    approval_date = mdb.DateTimeField()
+    approval_by = mdb.StringField() # dropdown-menu
+    # Legal continued(.pdf values)
+    pc_research_acc = mdb.BooleanField()
+    pc_data_sharing = mdb.BooleanField()
+    icfc_data_sharing = mdb.BooleanField()
+    expiration_date = mdb.DateTimeField()
+    ds_racc_notes = mdb.StringField()
+    legal_notes = mdb.StringField()
+
+    #Closure
+    status = mdb.StringField() # dropdown-menu
+    closure_notes = mdb.StringField()
+
+
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+def labelize(field_name):
+    return field_name.replace('_', ' ').title()
+
+def collab_model_form(model, only, field_args={}, **kwargs):
+     for field in only:
+        # generate/append field_args for each field; esp label
+        if field in field_args:
+            field_args[field]['label'] = labelize(field)
+        else:
+            field_args[field] = {'label': labelize(field)}
+
+     print (field_args)
+     return model_form(model, only=only, field_args=field_args, **kwargs)
 
 # Form representing the initiation view/stage
 InitiationForm = model_form(
@@ -40,6 +101,7 @@ InitiationForm = model_form(
                 'pi' : {'label': 'Principal Investigator'}}
     )
 
+DetailsForm = collab_model_form(Collaboration, ['neuropace_contact', 'sharing_method', 'study_title', 'dataset_description', 'phi_present', 'share_type', 'study_type', 'study_identifier', 'risk_level', 'accessories_needed', 'accessories_needed_language', 'single_multi_center', 'detail_comments'])
 
 # Controller and routes
 @app.route("/")
@@ -75,10 +137,10 @@ def initiation():
         form.save()
         return redirect('/')
     return render_template('init.html', form=form)
-
-@app.route("/details")
+# We need a way to track a specific collaboration through the flows
+@app.route("/details", methods=["GET","POST"])
 def details():
-    form = InitiationForm(request.form)
+    form = DetailsForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
         del(form.csrf_token)
         form.save()
