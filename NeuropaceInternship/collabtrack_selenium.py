20071bea8dd2e85c7329d00f9aebe6ecca526ada
lpbauer <lukephilip@gmail.com>
2017-05-18 16:35:02 -0700

    Three working integration tests using Selenium and PhantomJS

--- NeuropaceInternship/collabtrack_selenium.py
+++ NeuropaceInternship/collabtrack_selenium.py
@@ -14,7 +14,7 @@ class TestCase(LiveServerTestCase):
         return app
 
     def setUp(self):
-        self.driver = webdriver.phantomJS()
+        self.driver = webdriver.PhantomJS()
         self.driver.set_window_size(1120,550)
         self.driver.get(self.get_server_url())
         # conn = get_connection('test_db')
@@ -27,22 +27,24 @@ class TestCase(LiveServerTestCase):
         import_data()
     # Test visitation to the index page.
     def test_main_page(self):
-        response = self.driver.get(self.get_server_url())
-        self.assertEqual(response.status_code, 200)
-        assert b'NeuroPace Collaboration Tracker' in response.data
+        self.driver.get(self.get_server_url())
+        assert 'NeuroPace Collaboration Tracker' in self.driver.page_source
     #Login
     def test_user_login(self):
-        response = self.driver.get(self.get_server_url()+'/login/bauer123')
-        assert b'Logged in successfully.' in response.data
+        self.driver.get(self.get_server_url()+'/login/bauer123')
+        assert 'Logged in successfully.' in self.driver.page_source
 
     def test_ajax_index(self):
         #Navigate to the index page and ensure collab table has rendered.
-        response = self.driver.get(self.get_server_url())
-        assert b'15-000548' in response.data
+        self.driver.get(self.get_server_url())
+        self.driver.get(self.get_server_url()+'/login/bauer123')
+        assert 'Logged in successfully.' in self.driver.page_source
+
         #When Archive button is clicked, remove collaboration from list
         btn_archive = self.driver.find_element_by_id('archive_15-000548')
-        response = btn_archive.click()
-        assert b'15-000548' not in response.data
+
+        btn_archive.click()
+        assert '15-000548' not in self.driver.page_source
 
 
     # def new_collab(self):
